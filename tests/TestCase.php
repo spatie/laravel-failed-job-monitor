c48cd8bc6f9bae1ef2d9832e697f94a75768b590
Jolita Grazyte <jolita.grazyte@student.kdg.be>
2016-02-24 11:51:15 +0100

    delete unused methods

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -7,13 +7,6 @@ use Illuminate\Database\Schema\Blueprint;
 
 abstract class TestCase extends \Orchestra\Testbench\TestCase
 {
-    public function setUp()
-    {
-        parent::setUp();
-
-        $this->setUpDatabase($this->app);
-    }
-
     protected function getPackageProviders($app)
     {
         return [FailedJobsMonitorServiceProvider::class];
@@ -21,33 +14,10 @@ abstract class TestCase extends \Orchestra\Testbench\TestCase
 
     public function getEnvironmentSetUp($app)
     {
-        $app['config']->set('database.default', 'sqlite');
-        $app['config']->set('database.connections.sqlite', [
-            'driver' => 'sqlite',
-            'database' => $this->getTempDirectory().'/database.sqlite',
-            'prefix' => '',
-        ]);
+
         $app['config']->set('queue.default', 'sync');
 
-        $app['config']->set('app.key', '6rE9Nz59bGRbeMATftriyQjrpF7DcOQm');
+
     }
-
-    protected function setUpDatabase($app)
-    {
-        file_put_contents($this->getTempDirectory().'/database.sqlite', null);
-
-        $app['db']->connection()->getSchemaBuilder()->create('failed_jobs', function (Blueprint $table) {
-            $table->increments('id');
-            $table->text('connection');
-            $table->text('queue');
-            $table->longText('payload');
-            $table->timestamp('failed_at');
-
-        });
-    }
-
-    public function getTempDirectory($suffix = '')
-    {
-        return __DIR__.'/temp'.($suffix == '' ? '' : '/'.$suffix);
-    }
+    
 }
