1d2abac4a22db245d002d790531afd2feb3ce46a
Egor Talantsev <i.spyric@gmail.com>
2016-10-06 13:56:45 +0500

    * disable slack by default

--- config/laravel-failed-job-monitor.php
+++ config/laravel-failed-job-monitor.php
@@ -4,7 +4,7 @@ return [
     '*' => [
         'notifiable' => \App\User::class,
         'notification' => \Spatie\FailedJobMonitor\Notification::class,
-        'via' => ['mail', 'slack'],
+        'via' => ['mail'],
         //'filter' => 'canBeNotifiedAboutFailedJobs'
     ],
 ];
