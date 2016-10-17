d6e5f63f1156da000fe254b10ba397e4dab2de8d
Freek Van der Herten <freek@spatie.be>
2016-10-17 13:38:59 +0000

    Applied fixes from StyleCI

--- src/FailedJobNotifier.php
+++ src/FailedJobNotifier.php
@@ -5,19 +5,17 @@ namespace Spatie\FailedJobMonitor;
 use Illuminate\Queue\QueueManager;
 use Illuminate\Queue\Events\JobFailed;
 use Spatie\FailedJobMonitor\Exceptions\InvalidConfiguration;
-use Spatie\FailedJobMonitor\Exceptions\InvalidNotification;
 
 class FailedJobNotifier
 {
     public function register()
     {
         app(QueueManager::class)->failing(function (JobFailed $event) {
-
             $notifiable = app(config('laravel-failed-job-monitor.notifiable'));
 
             $notification = app(config('laravel-failed-job-monitor.notification'))->setEvent($event);
 
-            if (!$this->isValidNotificationClass($notification)) {
+            if (! $this->isValidNotificationClass($notification)) {
                 throw InvalidConfiguration::notificationClassInvalid(get_class($notification));
             }
             $notifiable->notify($notification);
