450590899ae419a02b74b274772b3df2146b2d10
patinthehat <patinthehat@users.noreply.github.com>
2021-03-25 15:29:32 +0000

    Fix styling

--- src/FailedJobMonitorServiceProvider.php
+++ src/FailedJobMonitorServiceProvider.php
@@ -2,8 +2,8 @@
 
 namespace Spatie\FailedJobMonitor;
 
-use Spatie\LaravelPackageTools\PackageServiceProvider;
 use Spatie\LaravelPackageTools\Package;
+use Spatie\LaravelPackageTools\PackageServiceProvider;
 
 class FailedJobMonitorServiceProvider extends PackageServiceProvider
 {
